afe577f92885f83d1867d33308b90670f3441e8e
a <a@a.a>
2019-01-25 22:03:48 +0800

    读入和写出IO端口的状态

---
+++ BookCodes/IOPort/IO_Traffic_Light.X/newmain.c
@@ -0,0 +1,102 @@
+// DEVCFG3
+// USERID = No Setting
+#pragma config FMIIEN = ON              // Ethernet RMII/MII Enable (MII Enabled)
+#pragma config FETHIO = ON              // Ethernet I/O Pin Select (Default Ethernet I/O)
+#pragma config PGL1WAY = ON             // Permission Group Lock One Way Configuration (Allow only one reconfiguration)
+#pragma config PMDL1WAY = ON            // Peripheral Module Disable Configuration (Allow only one reconfiguration)
+#pragma config IOL1WAY = ON             // Peripheral Pin Select Configuration (Allow only one reconfiguration)
+#pragma config FUSBIDIO = ON            // USB USBID Selection (Controlled by the USB Module)
+// DEVCFG2
+#pragma config FPLLIDIV = DIV_8         // System PLL Input Divider (8x Divider)
+#pragma config FPLLRNG = RANGE_34_68_MHZ// System PLL Input Range (34-68 MHz Input)
+#pragma config FPLLICLK = PLL_FRC       // System PLL Input Clock Selection (FRC is input to the System PLL)
+#pragma config FPLLMULT = MUL_128       // System PLL Multiplier (PLL Multiply by 128)
+#pragma config FPLLODIV = DIV_32        // System PLL Output Clock Divider (32x Divider)
+#pragma config UPLLFSEL = FREQ_24MHZ    // USB PLL Input Frequency Selection (USB PLL input is 24 MHz)
+// DEVCFG1
+#pragma config FNOSC = FRCDIV           // Oscillator Selection Bits (Fast RC Osc w/Div-by-N (FRCDIV))
+#pragma config DMTINTV = WIN_127_128    // DMT Count Window Interval (Window/Interval value is 127/128 counter value)
+#pragma config FSOSCEN = ON             // Secondary Oscillator Enable (Enable SOSC)
+#pragma config IESO = ON                // Internal/External Switch Over (Enabled)
+#pragma config POSCMOD = OFF            // Primary Oscillator Configuration (Primary osc disabled)
+#pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
+#pragma config FCKSM = CSECME           // Clock Switching and Monitor Selection (Clock Switch Enabled, FSCM Enabled)
+#pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
+#pragma config WDTSPGM = STOP           // Watchdog Timer Stop During Flash Programming (WDT stops during Flash programming)
+#pragma config WINDIS = NORMAL          // Watchdog Timer Window Mode (Watchdog Timer is in non-Window mode)
+#pragma config FWDTEN = ON              // Watchdog Timer Enable (WDT Enabled)
+#pragma config FWDTWINSZ = WINSZ_25     // Watchdog Timer Window Size (Window size is 25%)
+#pragma config DMTCNT = DMT31           // Deadman Timer Count Selection (2^31 (2147483648))
+#pragma config FDMTEN = ON              // Deadman Timer Enable (Deadman Timer is enabled)
+// DEVCFG0
+#pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
+#pragma config JTAGEN = ON              // JTAG Enable (JTAG Port Enabled)
+#pragma config ICESEL = ICS_PGx1        // ICE/ICD Comm Channel Select (Communicate on PGEC1/PGED1)
+#pragma config TRCEN = ON               // Trace Enable (Trace features in the CPU are enabled)
+#pragma config BOOTISA = MIPS32         // Boot ISA Selection (Boot code and Exception code is MIPS32)
+#pragma config FECCCON = OFF_UNLOCKED   // Dynamic Flash ECC Configuration (ECC and Dynamic ECC are disabled (ECCCON bits are writable))
+#pragma config FSLEEP = OFF             // Flash Sleep Mode (Flash is powered down when the device is in Sleep mode)
+#pragma config DBGPER = PG_ALL          // Debug Mode CPU Access Permission (Allow CPU access to all permission regions)
+#pragma config SMCLR = MCLR_NORM        // Soft Master Clear Enable bit (MCLR pin generates a normal system Reset)
+#pragma config SOSCGAIN = GAIN_2X       // Secondary Oscillator Gain Control bits (2x gain setting)
+#pragma config SOSCBOOST = ON           // Secondary Oscillator Boost Kick Start Enable bit (Boost the kick start of the oscillator)
+#pragma config POSCGAIN = GAIN_2X       // Primary Oscillator Gain Control bits (2x gain setting)
+#pragma config POSCBOOST = ON           // Primary Oscillator Boost Kick Start Enable bit (Boost the kick start of the oscillator)
+#pragma config EJTAGBEN = NORMAL        // EJTAG Boot (Normal EJTAG functionality)
+#pragma config CP = OFF                 // Code Protect (Protection Disabled)
+#include <xc.h>
+
+#define LED_ON  0
+#define LED_OFF 1
+#define LEDRED      LATAbits.LATA5
+#define LEDYELLOW   LATEbits.LATE5
+#define LEDGREEN    LATEbits.LATE6
+
+void delay()
+{
+    volatile unsigned long int i=0;
+    for(i=0;i<=265000;i++){asm("NOP");}
+}
+void main(void)
+{volatile i=0;
+    ANSELGbits.ANSG15=0;
+    TRISGbits.TRISG15=1;
+    TRISAbits.TRISA5=0;
+    LATAbits.LATA5=LED_OFF;
+    TRISEbits.TRISE5=0;
+    LATEbits.LATE5=LED_OFF;
+    TRISEbits.TRISE6=0;
+    LATEbits.LATE6=LED_OFF;
+    while(1)
+    {
+        if(PORTGbits.RG15==0)
+        {
+            switch(i)
+            {
+            case 0:
+                LEDRED      = LED_ON;
+                LEDYELLOW   = LED_OFF;
+                LEDGREEN    = LED_OFF;    
+                i++;
+                break;
+            case 1:
+                LEDRED      = LED_OFF;
+                LEDYELLOW   = LED_ON;
+                LEDGREEN    = LED_OFF;    
+                i++;
+                break;
+            case 2:
+                LEDRED      = LED_OFF;
+                LEDYELLOW   = LED_OFF;
+                LEDGREEN    = LED_ON;    
+                i=0;
+                break;
+            default :
+                i = 0;
+                break;
+            }
+            delay();
+        }
+    }
+    return;
+}
