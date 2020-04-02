ea0cc7b432bf071cddf8f3b1e71af369cd7ff2ac
a <a@a.a>
2020-04-03 00:19:54 +0800

    EFM搞定

---
+++ BookCodes/IIC/i2c_eeprom/firmware/pic32mk_gpl_curiosity_pro.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32mk_gpl_curiosity_pro configuration
+CND_ARTIFACT_DIR_pic32mk_gpl_curiosity_pro=dist/pic32mk_gpl_curiosity_pro/production
+CND_ARTIFACT_NAME_pic32mk_gpl_curiosity_pro=pic32mk_gpl_curiosity_pro.X.production.hex
+CND_ARTIFACT_PATH_pic32mk_gpl_curiosity_pro=dist/pic32mk_gpl_curiosity_pro/production/pic32mk_gpl_curiosity_pro.X.production.hex
+CND_PACKAGE_DIR_pic32mk_gpl_curiosity_pro=${CND_DISTDIR}/pic32mk_gpl_curiosity_pro/package
+CND_PACKAGE_NAME_pic32mk_gpl_curiosity_pro=pic32mkgplcuriositypro.x.tar
+CND_PACKAGE_PATH_pic32mk_gpl_curiosity_pro=${CND_DISTDIR}/pic32mk_gpl_curiosity_pro/package/pic32mkgplcuriositypro.x.tar
